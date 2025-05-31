150a7bb207114450f5e7b741cf0bee97ed047ba2
FrankBonanno <fb0023@mix.wvu.edu>
2025-05-31 00:49:21 -0400

    home layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import BookList from '@/components/BookList';
+import BookOverview from '@/components/BookOverview';
+
+const HomePage = () => (
+	<>
+		<BookOverview />
+		<BookList />
+	</>
+);
+
+export default HomePage;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button';
-
-const HomePage = () => (
-	<>
-		<Button>Click Me</Button>
-	</>
-);
-
-export default HomePage;
