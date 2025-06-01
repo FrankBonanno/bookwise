535b61e57e781f64776cd094748a72898a16dfc3
FrankBonanno <fb0023@mix.wvu.edu>
2025-05-31 23:23:12 -0400

    fixed auth redirect

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,12 @@
 import { auth } from '@/auth';
 import Header from '@/components/Header';
+import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
 
 const MainLayout = async ({ children }: { children: ReactNode }) => {
 	const session = await auth();
+
+	if (!session) redirect('/sign-in');
 
 	return (
 		<main className="root-container">
--- components/Header.tsx
+++ components/Header.tsx
@@ -18,10 +18,10 @@ const Header = ({ session }: { session: Session | null }) => {
 			<ul className="flex flex-row items-center gap-8">
 				<li>
 					<Link
-						href="/library"
+						href="/"
 						className={cn(
 							'text-base cursor-pointer capitalize',
-							pathname === '/library' ? 'text-light-200' : 'text-light-100',
+							pathname === '/' ? 'text-light-200' : 'text-light-100',
 						)}
 					>
 						Library
